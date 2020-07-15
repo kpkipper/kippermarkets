3abcbaf6e6643f21275416608e9883443dcfbb98
kritaporn Trisaktananon <kritaporn.trisaktananon@refinitiv.com>
2020-07-16 03:23:46 +0700

    select/update/delete

--- models/food.js
+++ models/food.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 const schema = new mongoose.Schema({
     id: {
-        type: Number
+        type: Number,
+        unique: true
     },
     name: {
         type: String
--- routes/index.js
+++ routes/index.js
@@ -25,18 +25,99 @@ router.post('/add',(req,res)=>{
     })
 })
 
-router.get('/',(req,res)=>{
-    res.end('E Boom E hia!');
-});
+router.get('/:id',(req,res)=>{
+    const id  = req.params.id;
+    Food.findOne({'id':id},(err,data)=>{
+        if(err){
+            res.status(500).json({
+                'error':err
+            })
+        } else {
+            res.status(200).json({
+                data: data
+            })
+        }
+    })
+})
 
-router.post('/',(req,res)=>{
-    const body = req.body
-    // const { body } = req
-    const message = body.message;
-    res.status(200).json({
-        message: "Boom E hia said: ", message
-    });
-});
+// put = update
+// post = new create
+
+router.put('/:id',(req,res)=>{
+    const id  = req.params.id;
+    const body = req.body;
+
+    Food.findOneAndUpdate({'id':id},body,(err,data)=>{
+        if(err){
+            res.status(500).json({
+                'error':err
+            })
+        } else {
+            res.status(200).json({
+                data: data
+            })
+        }
+    })
+})
+
+router.delete('/:id',(req,res)=>{
+    const id  = req.params.id;
+
+    Food.findOneAndDelete({'id':id},(err,data)=>{
+        if(err){
+            res.status(500).json({
+                'error':err
+            })
+        } else {
+            res.status(200).json({
+                data: data
+            })
+        }
+    })
+})
+
+router.delete('/byname/:name',(req,res)=>{
+    const name  = req.params.name;
+
+    Food.findOneAndDelete({'name':name},(err,data)=>{
+        if(err){
+            res.status(500).json({
+                'error':err
+            })
+        } else {
+            res.status(200).json({
+                data: data
+            })
+        }
+    })
+})
+
+router.get('/',(req,res)=>{
+    Food.find((err,data)=>{
+        if(err){
+            res.status(500).json({
+                'error':err
+            })
+        } else {
+            res.status(200).json({
+                data: data
+            })
+        }
+    })
+})
+
+// router.get('/',(req,res)=>{
+//     res.end('E Boom E hia!');
+// });
+
+// router.post('/',(req,res)=>{
+//     const body = req.body
+//     // const { body } = req
+//     const message = body.message;
+//     res.status(200).json({
+//         message: "Boom E hia said: ", message
+//     });
+// });
 
 router.get('/all',(req,res)=>{
     res.status(200).json({
